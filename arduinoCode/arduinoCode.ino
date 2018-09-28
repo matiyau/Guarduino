6783a4466c81f300f3f67618d0427c5b3b8f7ad2
Nishad Mandlik <mandliksg@gmail.com>
2018-09-28 13:22:34 +0530

    Ported to Leonardo

--- arduinoCode/arduinoCode.ino
+++ arduinoCode/arduinoCode.ino
@@ -4,18 +4,17 @@
 #define LOCK_OPN_CHAR 'o'
 #define LOCK_CLS_CHAR 'c'
 
-#define LOCK_PIN_A 3
-#define LOCK_PIN_B 4
+#define LOCK_PIN 13
 
-#define KEY_H0 12
-#define KEY_H1 11
-#define KEY_H2 10
-#define KEY_H3 9
+#define KEY_H0 A3
+#define KEY_H1 A2
+#define KEY_H2 A1
+#define KEY_H3 A0
 
-#define KEY_V0 8
-#define KEY_V1 7
-#define KEY_V2 6
-#define KEY_V3 5
+#define KEY_V0 9
+#define KEY_V1 10
+#define KEY_V2 11
+#define KEY_V3 6
 
 //Array Of Pins Giving Supply To Key Matrix
 uint8_t KEY_H_OUT[] = {KEY_H0,KEY_H1,KEY_H2,KEY_H3};
@@ -27,32 +26,28 @@ uint8_t KEY_V_IN[] = {KEY_V0,KEY_V1,KEY_V2,KEY_V3};
 uint8_t KEY_MAP[4][4] = {{1, 2, 3, 12}, {4, 5, 6, 13}, {7, 8, 9, 14}, {10, 0, 11, 15}};
 
 //Set The Passcode
-uint64_t passCode = 1033;
+uint32_t passCode = 1033;
 
 //Variable For User To Enter Code
-uint64_t userCode = 0;
+uint32_t userCode = 0;
 
 bool KEY_PRESSED_NOW = false;
 bool KEY_PRESSED_PREV = false;
 
-void lock() {
-  digitalWrite(LOCK_PIN_A, LOW);
-  digitalWrite(LOCK_PIN_B, LOW);
-  Serial.println("Closing");
-}
-
 void unlock() {
-  digitalWrite(LOCK_PIN_A, HIGH);
-  digitalWrite(LOCK_PIN_B, HIGH);
-  Serial.println("Opening");
+  digitalWrite(LOCK_PIN, LOW);
+  //Serial.println("Opening");
+  delay(1000);
+  digitalWrite(LOCK_PIN, HIGH);
+  //Serial.println("Closing");
 }
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   
-  pinMode(LOCK_PIN_A, OUTPUT);
-  pinMode(LOCK_PIN_B, OUTPUT);
+  pinMode(LOCK_PIN, OUTPUT);
+  digitalWrite(LOCK_PIN, HIGH);
 
   //Pin Config For KeyPad Matrix
   for (int i = 0; i<4; i++) {
@@ -63,7 +58,7 @@ void setup() {
     pinMode(KEY_V_IN[i], INPUT_PULLUP);
   }
   
-  Serial.println("Ready");
+  //Serial.println("Ready");
 }
 
 void loop() {
@@ -78,9 +73,6 @@ void loop() {
     if (CMD_MID_CHAR == LOCK_OPN_CHAR) {
       unlock();
     }
-    else if (CMD_MID_CHAR == LOCK_CLS_CHAR) {
-      lock();
-    }
   }
 
   
@@ -94,25 +86,24 @@ void loop() {
         KEY_PRESSED_NOW = true;
         if (!KEY_PRESSED_PREV) {
           uint8_t KEY = KEY_MAP[i][j];
-          if (KEY<=10) {
+          //Serial.println(KEY);
+          if (KEY<10) {
             userCode = userCode*10 + KEY;
           }
           else if (KEY==10) {
             if (userCode==passCode) {
               unlock();
             }
-            else {
-              userCode = 0;
-            }
+            userCode = 0;
           }
           else if (KEY==11) {
             userCode = userCode/10;
-          }
-          else if (KEY==12) {
-            lock();
           }
         }
       }
     }
   }
+  KEY_PRESSED_PREV = KEY_PRESSED_NOW;
+  KEY_PRESSED_NOW = false;
+  delay(50);  
 }
